9723456ee0c010d21b415b27fa6d3b29d0add593
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-03-06 22:46:03 +0500

    forth

--- src/components/contexts/authContext.js
+++ src/components/contexts/authContext.js
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const AuthContext = createContext({});
 
-export const server = "http://localhost:3001";
+export const server = "https://blog-back-bzo6.vercel.app";
 
 export function AuthContextProvider({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
